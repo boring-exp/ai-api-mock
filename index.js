0e9f867f1b1baa06c7c5337239053a41846a9a81
JinweiOS <jinwei.peng@beingthink.com>
2023-06-12 22:03:05 +0800

    feat: 修改为高德api模拟

--- index.js
+++ index.js
@@ -7,32 +7,13 @@ const failReply = require('./mock-data/weather-fail.json')
 const server = new Koa()
 const router = new KoaRouter
 
-router.get('/area-to-id', async (ctx) => {
-    const appCode = ctx.request.headers['authorization']
-    console.log(appCode)
+router.get('/v3/weather/weatherInfo', async (ctx) => {
     const queryP = ctx.query
-    if (queryP) {
-        const res = Object.keys(queryP).find(key => {
-            console.log(key)
-            return key === 'area' || key === 'areaCode'
-        })
-        console.log(res)
-        if (!res) {
-            failReply.showapi_res_error = '没有area或者areaCode'
-            ctx.body = failReply
-            return;
-        }
-    }
-    if (appCode) {
-        console.log(appCode)
-        const valid = `${appCode}`.split(' ')
-        console.log(valid)
-        if (valid[0] === 'APPCODE') {
-            ctx.body = successReply
-            return;
-        }
+    if (queryP.key && queryP.city && queryP.extensions) {
+        ctx.body = successReply
+        return;
     } else {
-        ctx.body = failReply;
+        ctx.body = failReply
     }
 })
 server.use(router.routes()).use(router.allowedMethods())
